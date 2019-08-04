dee8b6176a8ae27b4b64e07c16407de778566615
abykova <alex.94.girl@gmail.com>
2019-08-04 17:23:12 +0600

    [update] console command

--- console/controllers/SendController.php
+++ console/controllers/SendController.php
@@ -1,19 +1,46 @@
 <?php
 namespace console\controllers;
 
-use yii\helpers\Console;
 use yii\console\Controller;
 
-
 /**
- * Конфигурируем роли и разрешения.
  * @package app\commands
  */
 class SendController extends Controller
 {
+    public $arrayUser = [
+        '1' => 'user one',
+        '2' => 'user two',
+        '3' => 'user three',
+        '4' => 'user four',
+        '5' => 'user five',
+    ];
+
+    protected $arrayUserMoney = [
+        '1' => '57',
+        '2' => '',
+        '3' => '69',
+        '4' => '1',
+        '5' => '',
+    ];
+
 	public function actionInit()
 	{
-		echo 'hello this is command action';
-	}
-}
+        $arr = array($this->actionArrayUser());
 
+        foreach ($arr as  $name) {
+            for ($i = 0, $size = count($arr); $i < $size; ++$i) {
+                echo 'send ' . $name,PHP_EOL;
+            }
+        }
+	}
+
+	public function actionArrayUser()
+    {
+        foreach ($this->arrayUserMoney as $key => $value) {
+            if (!empty($value)){
+                return $this->arrayUser[$key];
+            }
+        }
+    }
+}
